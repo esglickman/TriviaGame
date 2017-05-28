b200da8dd81f56f82170042710ab1ac6eea54243
esglickman <esglickman@gmail.com>
2017-05-28 11:35:22 -0400

    pseudo code start

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+//Make an array of questions
+
+//make an object of answers
+
+//set a setTimeout to execute a function
+
+//The function should have: 30 sec timer display, display a question and 4 possible answers,
+//and when the 30 sec is up it will switch to a screen that gives answers for 7 seconds.
+
+//in the function: make if else statements (for the timer and for the answers clicked) AND
+//a screen that, if you choose the wrong answer it will tell you the correct one for 7 seconds
+
+//create function that keeps track of how many correct and incorrect answers are chose
+//display them at the end
+
+//make button that restarts the entire game without refreshing it.(Only display this when all questionsa are up.)
